48c9cf291f8264d83bd7b7633d78cb58078a99c5
gallardo034 <gallardoagustinm@gmail.com>
2023-07-17 19:11:09 -0300

    otro cambio

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-const express = require ('express');
-const path = require ('path');
-const app = express();
-
-const publicPath = path.resolve(__dirname, './public');
-
-app.use (express.static(publicPath));
-
-
-app.get ('/palabras'), (req, res) => {
-    res.send ('palabras random');
-};
-
-app.get ('/carrito', (req, res) => {
-    res.sendFile (path.resolve(__dirname, './views/carrito.html'))
-});
-
-app.get ('/checkout', (req, res) => {
-    res.sendFile (path.resolve(__dirname, './views/check-out.html'))
-});
-
-app.listen (3030, () => {
-    console.log('Servidor corriendo en el puerto 3030');
-=======
 const express = require('express');
 const app = express();
 
@@ -41,5 +16,12 @@ app.get('/login', (req,res)=>{
 
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
->>>>>>> 5dc2b617c9811a5e40ac27ad4281d07670d5d12e
+});
+
+app.get ('/carrito', (req, res) => {
+    res.sendFile (path.resolve(dirname, './views/carrito.html'))
+});
+
+app.get ('/checkout', (req, res) => {
+    res.sendFile (path.resolve(dirname, './views/check-out.html'))
 });
